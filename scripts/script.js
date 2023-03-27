f5bc7812802f8d44629b8d13f811a8653b35deaa
Oksana <oks49442780@gmail.com>
2023-03-27 20:06:00 +0200

    hw_3 'Математичні операції'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//hw_3 'Математичні операції'
+
+let x = prompt('first number');
+let y = prompt('second number');
+alert(`${Number(x)} + ${Number(y)} = ${Number(x) + Number(y)}
+       ${x} - ${y} = ${x - y}
+       ${x} * ${y} = ${x * y}
+       ${x} / ${y} = ${x / y}`);
