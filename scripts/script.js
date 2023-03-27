e8df0a3218476205edc3000cf19a6aaae2174569
Oksana <oks49442780@gmail.com>
2023-03-27 20:00:21 +0200

    hw_2 'Числа та рядки'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//hw_2
+
+const name = prompt('What is your name?');
+const age = prompt('How old are you?');
+let country = prompt('Where are you from?');
+alert(`${name} ${age} ${country}`);
+
+let a = 12345;
+console.log(a.toString().split('').join(' '));
