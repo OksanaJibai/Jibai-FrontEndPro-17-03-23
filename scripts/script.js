f1d54217a4c6b3ec125b027fe33228bb564a579c
Oksana <oks49442780@gmail.com>
2023-03-27 20:12:02 +0200

    hw_3-2 'Калькулятор'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//hw_3-2 'Калькулятор'
+
+
+let whatToDo = prompt('add, sub, mult, div - choose one of them');
+const a = prompt('first number');
+const b = prompt('second number');
+if (whatToDo==='add') {
+    alert (`${Number(a)} + ${Number(b)} = ${Number(a) + Number(b)}`);
+} else if(whatToDo==='sub'){
+    alert(`${a} - ${b} = ${a - b}`);
+} else if(whatToDo==='mult'){
+    alert(`${a} * ${b} = ${a * b}`);
+} else {
+    alert(`${a} / ${b} = ${a / b}`);
+}
