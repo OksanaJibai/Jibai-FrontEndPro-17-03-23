29a5878cfeb0e60b39d1c3a95757be5a60d54049
Oksana <oks49442780@gmail.com>
2023-03-27 21:47:08 +0200

    added events

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,28 @@
 
 //hw_3-2 'Калькулятор'
 
-
 let whatToDo = prompt('add, sub, mult, div - choose one of them');
 const a = prompt('first number');
 const b = prompt('second number');
-if (whatToDo==='add') {
+if (whatToDo ==='add' &&
+    typeof(a) === 'number' &&
+    typeof(b) === 'number'){
     alert (`${Number(a)} + ${Number(b)} = ${Number(a) + Number(b)}`);
-} else if(whatToDo==='sub'){
+} else if(whatToDo ==='sub'&&
+    typeof(a) === 'number' &&
+    typeof(b) === 'number'){
     alert(`${a} - ${b} = ${a - b}`);
-} else if(whatToDo==='mult'){
+} else if(whatToDo ==='mult'&&
+    typeof(a) === 'number' &&
+    typeof(b) === 'number'){
     alert(`${a} * ${b} = ${a * b}`);
-} else {
+} else if(whatToDo ==='div' &&
+    typeof(a) === 'number' &&
+    typeof(b) === 'number'){
     alert(`${a} / ${b} = ${a / b}`);
+} else if (typeof(a) !== 'number' ||
+    typeof(b) !== 'number'){
+    alert('a variable has not valid type');
+} else if (whatToDo === ' ') {
+    alert('');
 }
