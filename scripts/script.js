9996e2f3a19637ca83da43af18dcf44d88900693
Oksana <oks49442780@gmail.com>
2023-03-27 20:17:39 +0200

    hw_3-3 'Години в секунди'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//hw_3-3 'Години в секунди'
+
+let time = prompt('Вкажіть кількіть годин');
+let secondInHour = 60 * 60;
+let result =(`${time * secondInHour}`);
+
+if (time > 0 && time <= 24 || typeof time === 'number'){
+    alert(`Це ${result} секунд`);
+}else{
+    alert('Упс, щось пішло не так');
+}
