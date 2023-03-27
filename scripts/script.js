7f8150b4397be3e0fac389492fced4b5761546dd
Oksana <oks49442780@gmail.com>
2023-03-27 22:12:43 +0200

    hw_3-4 'Середнє значення'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//hw_3-4 'Середнє значення'
+
+let first = prompt('first number');
+let second = prompt('second number');
+let third = prompt('third number');
+
+if (typeof first === 'number' &&
+    typeof second === 'number' &&
+    typeof third === 'number'){
+    alert(`${(Number(first) + Number(second) + Number(third)) / 3}`);
+}
+
