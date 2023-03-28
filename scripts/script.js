381d2f94545678437cd7cc1e4488fae0be687633
Oksana <oks49442780@gmail.com>
2023-03-28 17:07:38 +0200

    hw_3-6 'switch...case'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//hw_3-6 'switch...case'
+
+let numOrStr = prompt('input number or string');
+
+switch (numOrStr) {
+    case null :
+        alert('ви скасували');
+        break;
+    case '':
+        alert('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        alert('number is Ba_NaN');
+        break;
+    default:
+        alert('OK!');
+}
