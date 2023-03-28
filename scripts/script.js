4740e117251446e661cf16651c82941a81e8bfad
Oksana <oks49442780@gmail.com>
2023-03-28 17:17:27 +0200

    change function

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,6 @@ let first = prompt('first number');
 let second = prompt('second number');
 let third = prompt('third number');
 
-if (typeof first === 'number' &&
-    typeof second === 'number' &&
-    typeof third === 'number'){
-    alert(`${(Number(first) + Number(second) + Number(third)) / 3}`);
-}
+alert(`${(parseInt(first) + parseInt(second) + parseInt(third)) / 3}`);
+
 
