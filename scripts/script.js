798ace44c3c2c7c94557f87a23c190f339ccee97
Oksana <oks49442780@gmail.com>
2023-03-29 20:01:41 +0200

    finish fixed init

--- scripts/script.js
+++ scripts/script.js
@@ -4,40 +4,31 @@
 
 const a = Number(prompt('Write first number'));
 const b = Number(prompt('Write second number'));
+let result = null;
+
+if(!a || !b){
+    alert('Невірне значення чисел');
+} else if(isNaN(a) || isNaN(b)){
+    alert('Вказаний невірний тип даних')
+} else{
+    Number(a) && Number(b);
+}
 const whatToDo = prompt('Choose: add, sub, mult, div');
 
-if (whatToDo === '' || whatToDo === null) {
-    alert('Ви не ввели ніодну із дій');
-
-} else if (whatToDo ==='add'){
-    alert (`${a} + ${b} = ${a + b}`);
-
-} else if(whatToDo ==='sub'){
-    alert(`${a} - ${b} = ${a - b}`);
-
-} else if(whatToDo ==='mult'){
-    alert(`${a} * ${b} = ${a * b}`);
-
-}else if(whatToDo ==='div' && b == 0){
+if(whatToDo ==='div' && b == 0) {
     alert('Ділення на нуль заборонено!');
-
-} else if(whatToDo ==='div'){
-    alert(`${a} / ${b} = ${a / b}`);
-
-}else if (typeof a !== 'number' || typeof b !== 'number') {
-    alert('Ви вказали невірне значення');
-
-}else {
-    alert('Щось пішло не так');
+} else if (whatToDo ==='div'){
+    result = a / b;
+    alert(`${a} / ${b} = ${result}`);
+}else if (whatToDo ==='add'){
+    result = a + b;
+    alert(`${a} + ${b} = ${result}`);
+}else if (whatToDo ==='sub'){
+    result = a - b;
+    alert(`${a} - ${b} = ${result}`);
+}else if (whatToDo ==='mult'){
+    result = a * b;
+    alert(`${a} * ${b} = ${result}`);
+}else if(!whatToDo || !result){
+    alert('Помилка введення оператора');
 }
-
-
-
-
-
-
-
-
-
-
-
