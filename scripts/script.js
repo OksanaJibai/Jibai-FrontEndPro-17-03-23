65fbe0e54d1094375db7e7dfd7c9ceb440e5b048
Oksana <oks49442780@gmail.com>
2023-04-03 01:40:35 +0200

    hw5-13/search in the Array

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+//hw5-13
+
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+//1
+const arrOne = arr.filter((item) => item > 0);
+let sum = arrOne.reduce(function(acc, item,){
+    return item + acc;
+}, 0);
+console.log(arrOne.length, sum);
+
+//2
+let min = Math.min(...arr);
+console.log(min, arr.findIndex((item) => item === min));
+
+//3
+let max = Math.max(...arr);
+console.log(max, arr.findIndex((item) => item === max));
+
+//4
+const arrTwo = arr.filter((value) => value < 0);
+console.log(arrTwo.length, arrTwo);
+
+//5
+const arrTree = arr.filter((value) => value % 2 !== 0 && value > 0);
+console.log(arrTree.length, arrTree);
+
+//6
+const arrFour = arr.filter((value) => value % 2 === 0 && value > 0);
+console.log(arrFour.length,arrFour);
+
+//7
+let totalOdd = arrFour.reduce((acc,item,) => acc + item);
+console.log(totalOdd);
+
+//8
+let totalEven = arrTree.reduce((acc,item,) => acc + item);
+console.log(totalEven);
+
+//9
+let subPositive = arrOne.reduce((acc,item,) => acc * item);
+console.log(subPositive);
+
+//10
+let newArr = arr.map(function (item){
+    if(item !== max) return 0;
+    return max;
+})
+console.log(newArr);
