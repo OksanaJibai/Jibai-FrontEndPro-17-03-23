db5f52131bb44161e8b3b97e69e8cb0aa8a4952a
Oksana <oks49442780@gmail.com>
2023-04-02 22:26:57 +0200

    hw_5-12/Array

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+//hw_5-12/Array
+
+let arr = [5, 10, 11, 55, 26, 13,];
+console.log(arr);
+
+function compareNumeric (a,b){
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+}
+console.log(arr.sort(compareNumeric));
+
+let arrTree = arr.splice(2,3);
+console.log(arr, arrTree);
