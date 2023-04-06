a08b5dadf3a7b4c7a173627acaec805148cd7aac
Oksana <oks49442780@gmail.com>
2023-04-06 17:07:03 +0200

    hw_6-15 'Function 'removeElement' '

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//hw_6-15 'removeElement'
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+function removeElement (arr, item){
+    let index = arr.indexOf(item);
+    return [
+        ...arr.slice(0, index),
+        ...arr.slice(index + 1)
+    ];
+}
+removeElement(array, 5);
+console.log(removeElement(array,5)); // [ 1, 2, 3, 4, 6, 7 ]
