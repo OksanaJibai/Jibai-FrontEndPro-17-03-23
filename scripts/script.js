f998d3917cd14a20863b61fbfc5dc09f45fd4538
Oksana <oks49442780@gmail.com>
2023-04-07 12:24:15 +0200

    hw_6 - 16 'function generateKey'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//hw_6 - 16 'function generateKey'
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey (elem, arr){
+    let str = '';
+for(let i = 0; i < elem; i++){
+    let position = Math.floor(Math.random() * arr.length);
+    str += arr.substring(position, position + 1);
+}
+return str;
+}
+
+const key = generateKey(16, characters);
+console.log(key, generateKey(26, characters));
