66607c1f5cb0dbb5ce3c239c75ddcb2ccadcf386
Oksana <oks49442780@gmail.com>
2023-04-20 23:58:39 +0200

    hw8-20/this chain

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//hw20 'this chain'
+
+class Ladder {
+    constructor(){
+        this.step = 0;
+    }
+    up(){
+        this.step++;
+        return this;
+    }
+    down(){
+        this.step--;
+        return this;
+    }
+    showStep() {
+        alert(this.step);
+        return this;
+    }
+}
+
+const ladder = new Ladder();
+ladder.up().up().down().showStep()
