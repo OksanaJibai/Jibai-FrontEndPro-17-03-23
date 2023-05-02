0ef89e977f3838a390625fcd0357d9f749a85a4f
Oksana <oks49442780@gmail.com>
2023-05-02 14:41:16 +0200

    hw 10-25/'Div-ghost'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const myInput = document.getElementById("myInput");
+const linkInfo = document.getElementById("link-info");
+
+// показуємо div при фокусуванні на input
+myInput.addEventListener("focus", function() {
+    linkInfo.style.display = "block";
+});
+
+// ховаємо div при втраті фокусу на input
+myInput.addEventListener("blur", function() {
+    linkInfo.style.display = "none";
+});
