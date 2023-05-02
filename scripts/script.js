19860c8819b070d784c164cd8efc023a603d65e1
Oksana <oks49442780@gmail.com>
2023-05-02 15:12:05 +0200

    hw10-26/'Table'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const table = document.createElement('table');
+for(let i = 1; i <= 10; i++){
+    const row = document.createElement('tr');
+    for(let e = 1; e <= 10; e++){
+        const cell = document.createElement('td');
+        const cellText = document.createTextNode((i - 1) * 10 + e);
+
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+    }
+    table.appendChild(row);
+}
+document.body.appendChild(table);
