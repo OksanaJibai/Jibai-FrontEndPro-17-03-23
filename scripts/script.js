eae81065c44c75a60a569bb25200fa484ba81927
Oksana <oks49442780@gmail.com>
2023-05-03 22:32:26 +0200

    hw10-28/'http:// protocol'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const protocol = "http://";
+const protocol2 = "https://";
+const form = document.querySelector("#form");
+const input = document.querySelector('#btn1');
+const btn = document.querySelector("button");
+let url;
+
+btn.addEventListener('click', () =>{
+    if(input.value.slice(0,7) !== protocol  &&
+        input.value.slice(0,8) !== protocol2){
+        url = protocol2 + input.value;
+        form.action = url;
+    }else{
+        form.action = input.value;
+    }
+});
