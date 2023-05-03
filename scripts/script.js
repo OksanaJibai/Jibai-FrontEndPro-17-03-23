c4eb001f28f3d0d5787cbc952978c24464b524f9
Oksana <oks49442780@gmail.com>
2023-05-03 12:04:42 +0200

    hw10-27/'Gallery'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Создаем массив с названиями изображений
+const images = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg", "5.jpeg", "6.jpeg", "7.jpeg"];
+
+// Получаем случайный индекс изображения
+const randomIndex = Math.floor(Math.random() * images.length);
+
+// Создаем элемент img и устанавливаем ему случайное изображение
+const img = document.createElement("img");
+img.src = "image/" + images[randomIndex];
+
+// Добавляем элемент img на страницу
+document.body.appendChild(img);
+
