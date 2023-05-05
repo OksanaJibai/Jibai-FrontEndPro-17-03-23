6948b10222ed31c7f177ee7481cb292515333334
Oksana <oks49442780@gmail.com>
2023-05-05 17:55:03 +0200

    hw10-28/'changed script'

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,14 @@
 'use strict';
 
-const protocol = "http://";
-const protocol2 = "https://";
-const form = document.querySelector("#form");
-const input = document.querySelector('#btn1');
-const btn = document.querySelector("button");
-let url;
+const link = document.querySelector('a');
+const protocol = 'https://';
+let btn = link.href;
 
-btn.addEventListener('click', () =>{
-    if(input.value.slice(0,7) !== protocol  &&
-        input.value.slice(0,8) !== protocol2){
-        url = protocol2 + input.value;
-        form.action = url;
-    }else{
-        form.action = input.value;
+link.addEventListener('click', (evt)=>{
+    evt.preventDefault();
+    let url = 'https://www.google.com/';
+    if (!btn.startsWith("http://") && !btn.startsWith("https://")) {
+        btn = protocol + btn;
     }
-});
+    document.location.href = btn;
+})
