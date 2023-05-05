5be6119e9442661b921da76a32553165197b7101
Oksana <oks49442780@gmail.com>
2023-05-05 22:42:30 +0200

    hw10-28/'changed script'

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,39 @@
 'use strict';
 
-const link = document.querySelector('a');
-const protocol = 'https://';
-let btn = link.href;
-
-link.addEventListener('click', (evt)=>{
-    evt.preventDefault();
-    let url = 'https://www.google.com/';
-    if (!btn.startsWith("http://") && !btn.startsWith("https://")) {
-        btn = protocol + btn;
+const protocol = "http://";
+const protocol2 = "https://";
+const form = document.getElementById('form');
+const input = document.querySelector('#btn1');
+const btn = document.querySelector("button");
+let url;
+btn.addEventListener('click', () =>{
+    if(input.value.slice(0,7) !== protocol  ||
+        input.value.slice(0,8) !== protocol2){
+        url = protocol2 + input.value;
+        form.action = url;
+    }else{
+        form.action = input.value;
     }
-    document.location.href = btn;
-})
+});
+
+
+// const link = document.querySelector('a');
+// console.dir(link);
+// console.log(link);
+// const btn = link.href;
+//
+//     link.addEventListener('click', (evt)=>{
+//         evt.preventDefault();
+//         let url;
+//         if (!btn.startsWith("http://") || !btn.startsWith("https://")) {
+//             url = "https://" + btn;
+//         }else {
+//             url = btn;
+//         }
+//
+//         window.open(url);
+//     })
+
+//це я пробувала код зробити на 2 посилання зроблених під кнопку(в яких введені посилання), але щось йде не так і не підставляється протокол
+//<a href="https://google.com/" class="btn1">Go Google</a>
+//     <a href="google.com/" class="btn1">Go Google</a>
