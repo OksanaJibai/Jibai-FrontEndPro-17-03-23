aa82c8f635a5485e25f0eb88c32dfd5d683a5dc1
Oksana <oks49442780@gmail.com>
2023-05-24 18:13:31 +0200

    hw37/ 'closer'

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+function getSum(){
+    let sum = 0;
+    return function (num){
+        sum += num;
+        return sum;
+    };
+}
+let sum = getSum();
+sum(3);
+sum(5);
+sum(10);
