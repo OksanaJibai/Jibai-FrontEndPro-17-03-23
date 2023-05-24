8f236811486b17dc8bf7fc9a79df3606cc693e69
Oksana <oks49442780@gmail.com>
2023-05-24 22:37:36 +0200

    hw38/closer V2

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function getRandomNumber (){
+    const generateNumbers = [];
+    function generateNumber(){
+        let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+        while (generateNumbers.includes(randomNumber)) {
+            randomNumber = Math.floor(Math.random() * 100) + 1;
+        }
+        generateNumbers.push(randomNumber);
+
+        return randomNumber;
+    }
+    return generateNumber();
+}
+
+const generateRandomNumber = getRandomNumber();
