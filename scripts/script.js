a9b900264288a3b497760e782de0a68ccb7ef286
Oksana <oks49442780@gmail.com>
2023-05-25 23:13:56 +0200

    hw43/ DOM-V3

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function generateList(array){
+    const list = document.createElement('ul');
+    for(let i = 0; i < array.length; i++){
+        const item = document.createElement('li');
+        if(Array.isArray(array[i])){
+            item.appendChild(generateList(array[i]));
+        }else {
+            const textItem = document.createTextNode(array[i]);
+            item.appendChild(textItem);
+        }
+        list.appendChild(item);
+    }
+    return list;
+}
+const result = generateList([1,2, [1.1,1.2,1.3] ,3]);
+console.log(result);
