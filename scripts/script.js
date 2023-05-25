6b2c315792451eb0e1611fb3946aeab55cd9c096
Oksana <oks49442780@gmail.com>
2023-05-25 16:34:04 +0200

    hw44/ DOM-V4

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const container = document.getElementById('wrap');
+function createTable(){
+    const table = document.createElement('table');
+    for(let i = 1; i <= 10; i++){
+        const row = document.createElement('tr');
+        for(let j = 1; j <= 10; j++){
+            const cell = document.createElement('td');
+            const textOfCell = document.createTextNode((i - 1) * 10 + j);
+
+            cell.appendChild(textOfCell);
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+
+    }
+    container.appendChild(table);
+    return createTable;
+}
+createTable();
