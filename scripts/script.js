2820ebd4122ad235bd675ee7e0582e8ef9ce2563
Oksana <oks49442780@gmail.com>
2023-06-18 13:24:20 +0200

    hw56-added function

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const arr = [1, 2, 3, 4];
+
+function iterator (arr) {
+    let i = 0;
+    return{
+        next: () =>{
+            if(i < arr.length){
+                return {value : arr[i++], done : false};
+            }else{
+                return {value : undefined, done : true};
+            }
+        }
+    };
+}
+const it = iterator(arr);
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
