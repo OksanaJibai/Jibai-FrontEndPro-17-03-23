31e4c57e3ffc135136b2a3afa0ed78e76669f093
Oksana <oks49442780@gmail.com>
2023-07-01 23:22:56 +0200

    hw66 - Map - init commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+(function (){
+
+    const goods = [['футболка', 15], ['шорты', 25], ['носки', 5]];
+
+    const map = new Map();
+
+   for(const [item, price] of goods){
+       map.set(item, price);
+   }
+
+   let sumPrice = 0;
+
+   for(const price of map.values()){
+       sumPrice += price;
+   }
+
+    console.log(sumPrice);
+})()
