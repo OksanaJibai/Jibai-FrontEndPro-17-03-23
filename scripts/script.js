f8784b81252a61627abc4f8f86a94a9ba3763420
Oksana <oks49442780@gmail.com>
2023-07-01 23:34:47 +0200

    hw67 - Set - init commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+(function (){
+
+    const string = "abracadabra";
+
+    const setString = new Set();
+
+    for(const symbol of string){
+        setString.add(symbol);
+    }
+    console.log(setString.size);
+})()
