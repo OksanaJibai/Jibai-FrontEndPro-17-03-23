41612f76bbcd06ee29d2272a3838d3dfef0ef184
Oksana <oks49442780@gmail.com>
2023-07-02 20:56:40 +0200

    hw68 - WeakMap - init commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+(function (){
+
+    const student = { name: 'Иван Иванов' };
+    const grades = { math: 90, fiz: 85, eng: 95 };
+
+    const studentMarks = new WeakMap();
+
+    function saveGrades(student, grades){
+        if(!studentMarks.has(student)){
+            studentMarks.set(student, grades);
+        }
+        return studentMarks.get(student);
+    }
+
+    console.log(saveGrades(student, grades));
+})()
