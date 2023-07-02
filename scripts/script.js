4c712360f25469ecbcd983187193dcac8542309e
Oksana <oks49442780@gmail.com>
2023-07-02 21:17:40 +0200

    hw69 - WeakSet - init commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+(function (){
+    const player1 = { id: 1, name: 'Иван', level: 10 };
+
+    const levelSet = new WeakSet();
+
+    function addPlayerToLevelSet(player){
+        levelSet.add(player);
+    }
+
+
+    const player3 = { id: 2, name: 'Иван', level: 11 };
+
+    addPlayerToLevelSet(player1);
+    addPlayerToLevelSet(player1);
+    addPlayerToLevelSet(player3);
+
+})()
