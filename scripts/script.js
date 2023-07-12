4dd795681558416c5ee4d7bc614074bdcb7173b6
Oksana <oks49442780@gmail.com>
2023-07-13 00:48:23 +0200

    hw71 - fixed func calculate and calculatePrice

--- scripts/script.js
+++ scripts/script.js
@@ -1,77 +1,70 @@
 "use strict";
 
-class Hamburger {
-    constructor(size, staffing) {
-        this.size = size;
-        this.staffing = staffing;
-    }
-    topping = {
-        price : 0,
-        kal : 0,
-    };
-    //маленький (50 тугриків, 20 калорій); великий (100 тугриків, 40 калорій).
-    static SIZE_SMALL = {
-        price : 50,
-        kal : 20,
-    };
-    static SIZE_LARGE = {
-        price : 100,
-        kal : 40,
-    };
-    //сиром (+ 10 тугриків, + 20 калорій); салатом (+ 20 тугриків, + 5 калорій); картоплею (+ 15 тугриків, + 10 калорій).
-    static STUFFING_CHEESE = {
-        price : 10,
-        kal : 20,
-    };
-    static STUFFING_SALAD = {
-        price : 20,
-        kal : 5,
-    };
-    static STUFFING_POTATO = {
-        price : 15,
-        kal : 10,
-    };
-    //посипати приправою (+15 тугриків, 0 калорій) - полити майонезом (+ 20 тугриків, +5 калорій).
-    static TOPPING_MAYO = {
-        price : 20,
-        kal : 5,
-    };
-    static TOPPING_SAUCE = {
-        price : 15,
-        kal : 0,
-    };
-    addTopping(toppingType){
-        this.topping.price += toppingType.price;
-        this.topping.kal += toppingType.kal;
-        return this.topping;
-    }
-    calculate(){
-        let sumOfKal = 0;
-        if(this.staffing){
-            return ( sumOfKal += this.size.kal + this.staffing.kal + this.topping.kal );
+(function (){
+    class Hamburger {
+        constructor(size, staffing) {
+            this.size = size;
+            this.staffing = staffing;
+            this.topping = {
+                price : 0,
+                kal : 0,
+            };
         }
-        return ( sumOfKal += this.size.kal + this.topping.kal );
-    }
-
-    calculatePrice (){
-        let sumOfPrise = 0;
-        if(this.staffing){
-            return ( sumOfPrise += this.size.price + this.staffing.price + this.topping.price );
-        }else{
-            return ( sumOfPrise += this.size.price + this.topping.price );
+        static SIZE_SMALL = {
+            price : 50,
+            kal : 20,
+        };
+        static SIZE_LARGE = {
+            price : 100,
+            kal : 40,
+        };
+        static STUFFING_CHEESE = {
+            price : 10,
+            kal : 20,
+        };
+        static STUFFING_SALAD = {
+            price : 20,
+            kal : 5,
+        };
+        static STUFFING_POTATO = {
+            price : 15,
+            kal : 10,
+        };
+        static TOPPING_MAYO = {
+            price : 20,
+            kal : 5,
+        };
+        static TOPPING_SAUCE = {
+            price : 15,
+            kal : 0,
+        };
+        addTopping(toppingType){
+            this.topping.price += toppingType.price;
+            this.topping.kal += toppingType.kal;
+            return this.topping;
         }
+        calculate(){
+            return  this.size.kal + (this.staffing ? this.staffing.kal : 0) + this.topping.kal;
+        }
+
+        calculatePrice (){
+            return   this.size.price + (this.staffing ? this.staffing.price : 0) + this.topping.price;
+        }
+
     }
 
-}
+    const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO);
+    hamburger.addTopping(Hamburger.TOPPING_MAYO);
+    console.log('Calories: ' + hamburger.calculate());
+    console.log('Price: ' + hamburger.calculatePrice());
+    hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+    console.log('Price with sauce: ' + hamburger.calculatePrice());
 
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_POTATO);
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
-console.log('Calories: ' + hamburger.calculate());
-console.log('Price: ' + hamburger.calculatePrice());
-hamburger.addTopping(Hamburger.TOPPING_SAUCE);
-console.log('Price with sauce: ' + hamburger.calculatePrice());
-
-const cheeseburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE);
-cheeseburger.addTopping(Hamburger.TOPPING_SAUCE);
-console.log('Calories: ' + cheeseburger.calculate ());
-console.log('Price: ' + cheeseburger.calculatePrice());
+    const cheeseburger = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE);
+    cheeseburger.addTopping(Hamburger.TOPPING_SAUCE);
+    console.log('Calories: ' + cheeseburger.calculate ());
+    console.log('Price: ' + cheeseburger.calculatePrice());
+    cheeseburger.addTopping(Hamburger.TOPPING_MAYO);
+    console.log('Calories with mayo: ' + cheeseburger.calculate ());
+    console.log('Price with mayo: ' + cheeseburger.calculatePrice());
+})()
