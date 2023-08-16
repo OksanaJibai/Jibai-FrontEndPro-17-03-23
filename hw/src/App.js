bb634e930e52ac07cc9150b78ebc4b9e6eae34b5
Oksana <oks49442780@gmail.com>
2023-08-16 23:18:10 +0200

    init

--- hw/src/App.js
+++ hw/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Definitions from "./Definitions/Definitions";
+import {definitions} from "./data/data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className= "container p-5">
+    <Definitions data={definitions}/>
+  </div>
 }
 
 export default App;
