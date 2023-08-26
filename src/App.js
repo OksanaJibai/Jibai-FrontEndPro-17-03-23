627b9c7c1ae756025cf8f1c85e80758c5c5a8412
Oksana <oks49442780@gmail.com>
2023-08-26 23:26:40 +0200

    fixed BtnGroup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,9 @@ import BtnGroup from "./components/BtnGroup";
 
 function App() {
 
-    const buttonsData =[
-        {"id" : 1, "buttonText" : 'left'},
-        {"id" : 2, "buttonText" : 'right'}
-    ];
   return (
           <div className="container pt-3">
-              <BtnGroup buttonsData={buttonsData}/>
+              < BtnGroup />
           </div>
   );
 }
--- src/components/BtnGroup.jsx
+++ src/components/BtnGroup.jsx
@@ -1,21 +1,51 @@
 import React from "react";
-import Button from "./Button";
+import classNames from "classnames";
 
 class BtnGroup extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeBtn: null
+        }
+    }
+
+    setActive = (btn) =>{
+        this.setState((state) => {
+            const {button} = state;
+            return { activeBtn : button === btn ? null : btn};
+        })
+    }
+    onClickLeft =() => this.setActive('left');
+
+    onClickRight =() => this.setActive('right');
+
     render(){
+        const {activeBtn} = this.state;
+        const btnClassRight = classNames('btn btn-danger',{
+            "active" : activeBtn === "right"
+        });
 
-        const {buttonsData} = this.props;
-
-        return (
+        const btnClassLeft = classNames('btn btn-primary',{
+            "active" : activeBtn === "left"
+        });
+        return(
             <div className="btn-group" role="group">
-                {buttonsData.map(element =>{
-                    const {id, buttonText} = element;
-                    return <Button key={id} buttonText={buttonText}/>
-                })
-                }
+                <button type="button"
+                        className={btnClassLeft}
+                        onClick={this.onClickLeft}
+                >RIGHT
+                </button>
+                <button type="button"
+                        className={btnClassRight}
+                        onClick={this.onClickRight}
+                >LEFT
+                </button>
             </div>
         )
     }
 }
+BtnGroup.defaltProps={
+    state: null
 
+}
 export default BtnGroup;
