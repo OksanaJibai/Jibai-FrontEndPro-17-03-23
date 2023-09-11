e27354c282fc245bdf4e3b49e36f8bbfcae06df9
Oksana <oks49442780@gmail.com>
2023-09-11 18:37:18 +0200

    add hw-93

--- src/components/PostCatalog.jsx
+++ src/components/PostCatalog.jsx
@@ -1,29 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-class PostCatalog extends React.Component{
+const PostCatalog = () => {
 
-    state = {posts : []};
+    const [posts, setPosts] = useState([]);
 
-    async componentDidMount(){
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const posts = await res.json();
-        this.setState({posts : [...posts]})
-    }
-    render(){
-        const {posts} = this.state;
-        return (
-            <div className="posts">
-                <ul className="posts__list">
-                    {posts.map(({id, title, body}) =>
-                        <li className="posts_single-post" key={id}>
-                          <h3 className="posts__post-title">{title}</h3>
-                          <p className="posts__post-description">{body}</p>
-                        </li>)
-                    }
-                </ul>
-            </div>
-        );
-    }
+    useEffect(()=>{
+        const fetchPosts = async () =>{
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const posts = await res.json();
+            setPosts(posts);
+        };
+        fetchPosts().then();
+    },[])
+
+    return (
+        <div className="posts">
+            <ul className="posts__list">
+                {posts.map(({id, title, body}) =>
+                    <li className="posts_single-post" key={id}>
+                        <h3 className="posts__post-title">{title}</h3>
+                        <p className="posts__post-description">{body}</p>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+
 }
 
 export default PostCatalog;
